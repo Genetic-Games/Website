69ba094150429e2a43493148b708c658a4106737
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2021-01-02 13:56:56 -0500

    Adding application path environment variable usage

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ var contact = require(path.join(customModulePath, 'contact.js'));
 // Setup Page Handling
 const staticFilesPath = path.join(__dirname, 'public');
 const viewsFilesPath = path.join(__dirname, 'views');
+const applicationPath = process.env.APPLICATION_PATH || '';
 
 var app = express();
 app.use(express.static(staticFilesPath))
@@ -34,22 +35,22 @@ app.set('view engine', 'vash')
  .set('views', viewsFilesPath);
 
 // Home Logic
-app.get('/', home.getHomePage);
-app.get('/home', home.getHomePage);
+app.get(applicationPath + '/', home.getHomePage);
+app.get(applicationPath + '/home', home.getHomePage);
 
 // General Purpose Page Logic
-app.get('/about', about.getAboutPage);
-app.get('/news', news.getNewsPage);
-app.get('/games', games.getGamesPage);
-app.get('/credits', credits.getCreditsPage);
-app.get('/contact', contact.getContactPage);
+app.get(applicationPath + '/about', about.getAboutPage);
+app.get(applicationPath + '/news', news.getNewsPage);
+app.get(applicationPath + '/games', games.getGamesPage);
+app.get(applicationPath + '/credits', credits.getCreditsPage);
+app.get(applicationPath + '/contact', contact.getContactPage);
 
 // Games Page Logic
-app.get('/games/Evolution', games.getGamePageForEvolution);
+app.get(applicationPath + '/games/Evolution', games.getGamePageForEvolution);
 
 // Error Handling
-app.use('/access_denied', error.handleAccessNotAllowed);
-app.use('/error', error.handleExpectedError);
+app.use(applicationPath + '/access_denied', error.handleAccessNotAllowed);
+app.use(applicationPath + '/error', error.handleExpectedError);
 app.use(error.handlePageNotFound);
 app.use(error.handleUnexpectedError);
 
