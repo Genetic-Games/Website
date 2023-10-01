2fca8d81ca8a34df0891fb578997ae191091cc5a
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2023-10-01 13:11:23 -0400

    Fix Strict-Transport-Security header and remove X-Powered-By header

--- modules/security.js
+++ modules/security.js
@@ -4,6 +4,7 @@ var path = require('path'); // URI and local file paths
 // Security Header Logic
 exports.addSecurityHeaders = function(req, res, next)
 {
-    res.setHeader('Strict-Transport-Security', 'Strict-Transport-Security: max-age=31536000; includeSubDomains')
+    res.removeHeader('X-Powered-By')
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
     next();
 }
